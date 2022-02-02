09103214a8043aa223e96a8211ddc01dc94618c5
nazarko310 <nazar2000pok@gmail.com>
2022-02-02 22:12:09 +0200

    lessons1_hw

---
+++ main.js
@@ -0,0 +1,191 @@
+// // - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// //     Вивести кожну змінну за допомогою: console.log , alert, document.write
+//
+// let hello = 'hello';
+// let owu = 'owu';
+// let com = 'com';
+// let ua = 'ua';
+// let number = 1;
+// let number1 = 10;
+// let number2 = -999;
+// let number3 = 123;
+// let number4 = 3.14;
+// let number5 = 2.7;
+// let number6 = 16;
+// let trues = true;
+// let falses = false;
+//
+// console.log(hello);
+// console.log(owu);
+// console.log(com);
+// console.log(ua);
+// console.log(number);
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+// console.log(number4);
+// console.log(number5);
+// console.log(number6);
+// console.log(trues);
+// console.log(falses);
+//
+//
+// alert(hello);
+// alert(owu);
+// alert(com);
+// alert(ua);
+// alert(number);
+// alert(number1);
+// alert(number2);
+// alert(number3);
+// alert(number4);
+// alert(number5);
+// alert(number6);
+// alert(trues);
+// alert(falses);
+//
+// document.writeln(hello);
+// document.writeln(owu);
+// document.writeln(com);
+// document.writeln(ua);
+// document.writeln(number);
+// document.writeln(number1);
+// document.writeln(number2);
+// document.writeln(number3);
+// document.writeln(number4);
+// document.writeln(number5);
+// document.writeln(number6);
+// document.writeln(trues);
+// document.writeln(falses);
+
+// // - Переприсвоїти кожній змінній з завдання значення на довільне.
+// //     Вивести кожну змінну за допомогою: console.log , alert, document.write
+//
+// hello = 'hi';
+// owu = 'hhh';
+// com = 'kk';
+// ua = 3;
+// number = 6;
+// number1 = 75;
+// number2 = 7657;
+// number3 = -256;
+// number4 = 2.5854;
+// number5 = 49874;
+// number6 = 4894;
+// trues = false;
+// falses = true;
+//
+// console.log(hello);
+// console.log(owu);
+// console.log(com);
+// console.log(ua);
+// console.log(number);
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+// console.log(number4);
+// console.log(number5);
+// console.log(number6);
+// console.log(trues);
+// console.log(falses);
+//
+//
+// alert(hello);
+// alert(owu);
+// alert(com);
+// alert(ua);
+// alert(number);
+// alert(number1);
+// alert(number2);
+// alert(number3);
+// alert(number4);
+// alert(number5);
+// alert(number6);
+// alert(trues);
+// alert(falses);
+//
+// document.writeln(hello);
+// document.writeln(owu);
+// document.writeln(com);
+// document.writeln(ua);
+// document.writeln(number);
+// document.writeln(number1);
+// document.writeln(number2);
+// document.writeln(number3);
+// document.writeln(number4);
+// document.writeln(number5);
+// document.writeln(number6);
+// document.writeln(trues);
+// document.writeln(falses);
+
+// // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+//
+// let firstName = 'Назарій';
+// let secondName = 'Покровець';
+// let middleName = 'Ігорович';
+//
+// console.log(`${secondName} ${firstName} ${middleName}`);
+
+// // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+// //     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+//
+//
+// let firstName = prompt('Ваше ім`я');
+// let secondName = prompt('Ваше прізвище');
+// let age = prompt('Скільки вам років');
+//
+// console.log(`Вітаю ${firstName} ${secondName}. Тобі ${age} років.`);
+
+// // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// //     let a = 100; let b = '100'; let c = true;
+//
+//
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+
+
+// // - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+// //     В однакових виразах не використовувати однакові оператори!!!
+//
+// //     5 ? 6 -> true
+// // 5 ? 6 -> false
+// // 5 ? 6 -> false
+// // 5 ? 6 -> false
+// // 10 ? 10 -> true
+// // 10 ? 10 -> true
+// // 10 ? 10 -> false
+// // 10 ? 10 -> false
+// // 10 ? 10 -> false
+// // 123 ? '123' -> false
+// // 123 ? '123' -> true
+//
+// console.log(5 < 6);
+// console.log(5 > 6);
+// console.log(5 === 6);
+// console.log(5 >= 6);
+// console.log(10 == 10);
+// console.log(10 === 10);
+// console.log(10 > 10);
+// console.log(10 > 10);
+// console.log(10 !== 10);
+// console.log(123 > 123);
+// console.log(123 === 123);
+
+
+// // - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+// let str = "20";
+// let a = 5;
+//
+//
+// document.write(str + a + "<br/>");//205
+//
+//
+// document.write(str - a + "<br/>"); //15
+// document.write(str * "2" + "<br/>");//40
+// document.write(str / 2 + "<br/>"); //10
+
