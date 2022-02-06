aa1d0740a0612110d2b93a325e46e062f7d025f5
nazarko310 <nazar2000pok@gmail.com>
2022-02-06 23:09:09 +0200

    lessons3_add3

---
+++ main.js
@@ -0,0 +1,44 @@
+// // 1. Створити пустий масив та :
+// //     a. заповнити його 50 парними числами за допомоги циклу.
+// //     b. заповнити його 50 непарними числами за допомоги циклу.
+// //     c. Заповнити масив 20ма рандомними числами. (Google: Generate random number JS)
+// // d. Заповнити масив 20ма рандомними чисалами в діапазоні від 8 до 732 (Google: Generate random number JS)
+//
+//
+// let array = [];
+//
+// // a. заповнити його 50 парними числами за допомоги циклу.
+// // for (let i = 0; i < 50; i++) {
+// //     array[0] = 2;
+// //     array[i+1] = array[i] + 2
+// //     console.log(array[i])
+// // }
+//
+// // // b. заповнити його 50 непарними числами за допомоги циклу.
+// //
+// // for (let i = 0; i < 50; i++) {
+// //     array[0] = 1;
+// //     array[i+1] = array[i] + 2
+// //     console.log(array[i])
+// // }
+//
+// // // Заповнити масив 20ма рандомними числами. (Google: Generate random number JS)
+// //
+// // for (let i = 0; i < 20; i++) {
+// //     let arrayElement = array[i];
+// //
+// //     arrayElement = Math.floor(Math.random() * 10)
+// //     console.log(arrayElement)
+// //
+// // }
+//
+// // // d. Заповнити масив 20ма рандомними чисалами в діапазоні від 8 до 732 (Google: Generate random number JS)
+// //
+// // for (let i = 0; i < 20; i++) {
+// //     let arrayElement = array[i];
+// //
+// //     arrayElement = Math.floor(Math.random() * (732 - 8))
+// //     console.log(arrayElement)
+// //
+// // }
+
