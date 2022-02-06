33a173f8daf0e0591dadd2f27fd6b9bab1015877
nazarko310 <nazar2000pok@gmail.com>
2022-02-06 20:36:55 +0200

    lessons3_hw

--- main.js
+++ main.js
@@ -1,51 +1,190 @@
-// // - Нам дано масив друзів let friends = ["Ivan", "Slavik", "Grisha", "Lera", "Ira"]; Потрібно зробити перевірку якщо
-// // кількість елементів у масиві більше або дорівнює 3, то виводиться повідомлення про те, що це великий масив, в якому  3 і більше елементи.
-// //     Інакше слід вивести на екран повідомлення про те, що це маленький масив, в якому менше 3-х елементів.
+// // --створити масив з:
+// //     - з 5 числових значень
+// // - з 5 стічкових значень
+// // - з 5 значень стрічкового, числового та булевого типу
+// // - та вивести його в консоль
 //
-// let friends = ["Ivan","Slavik", "Grisha", "Karina", "Ira"];
 //
-// if (friends.length >= 3) {
-//     console.log('Великий масив в якому 3 більше елементів');
+// let array = [1, 2, 3, 4, 5];
+// console.log(array);
 //
-// } else {
-//     console.log('Маленький масив в якому менше 3 елементів');
+// let arrayOne = ['Vitaliy', 'Karina', 'Masha', 'Maks', 'Dimas'];
+// console.log(arrayOne);
+//
+// let arrayTwo = ['Vitaliy', 'Karina', 1, 'Masha', 55, 'Maks', 'Dimas', 648, 48, true, false, 489, 484];
+// console.log(arrayTwo);
+
+// // -- Створити пустий масив. Наповнити його будь-якими значеннями звертаючись до конкретного індексу. Вивести в консоль
+//
+// let array = [];
+//
+// array[0] = 2;
+// array[1] = 'Vasia';
+// array[2] = true;
+// array[3] = 'Maks';
+//
+// console.log(array)
+
+//
+// // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+//
+// for (let i = 0; i < 10; i++) {
+//     document.write('<div>Hallo world</div>')
 // }
 
-// // - Маємо 3 числа і між ним  нам потрібно знайти те що посередині.
-// //     Знайти, яке з них є середнім (більше одного, але менше за інше). Перевірити, чи знаходиться перше число між двома іншими.
+// // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
 //
-// let numberOne = prompt('Введіть число');
-// let numberTwo = prompt('Введіть число');
-// let numberThree = prompt('Введіть число');
+// for (let i = 1; i < 11; i++) {
+//     document.write(`<div>Hello I'm № ${i}</div>`)
 //
-// if(numberOne>numberTwo&&numberOne<numberThree){
-//     console.log(numberOne);
-// }else if (numberTwo > numberOne && numberTwo < numberThree) {
-//     console.log(numberTwo);
-// } else {
-//     console.log(numberThree)
 // }
 
-// // - Перепишіть конструкцію if з використанням умовного оператора '?':
-// // let result;
-// // if (a + b < 4) {
-// //     result = 'Мало';
-// // } else {
-// //     result = 'Багато';
-// // }
-//
-// let a=1;
-// let b=5;
-// let result;
-//
-// (a + b) < 4 ? (result = 'Мало') : (result = 'Багато');
+// // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+// let i = 1;
+// while (i < 21) {
+//     document.write(`<h1>Hello</h1>`)
+//     i++;
+// }
 
-// // - Маємо будь яке число від -100 , 0 , +100 потрібно перевірити яким є число: позитивним, негативним або нулем
-// // напишіть це тернарним оператором
+// // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
 //
-// let number = prompt('Введіть число від -100 до +100');
+// let i = 1;
+// while (i < 21) {
+//     document.write(`<h1>Hello № ${i}</h1>`)
+//     i++;
+// }
+
+// // - Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
 //
-// (number < '0') ? (console.log('Негативний')) :
-//     (number > '0') ? (console.log('Позитивний')) :
-//         console.log('Нуль')
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //
+// for (let i = 0; i < array.length; i++) {
+//     const arrayElement = array[i];
+//     console.log(arrayElement)
+//
+// }
+
+// // - Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
+//
+// let array = ['Vasia', 'Katia', 'Petro', 'Bart', 'Karina', 'Dimas', 'Nazar', 'Vitaliy', 'Masha', 'Marta'];
+//
+// for (let i = 0; i < array.length; i++) {
+//     const arrayElement = array[i];
+//     console.log(arrayElement)
+//
+// }
+
+// // - Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+//
+// let array = ['Vasia', 1, 'Petro', 'Bart', true, 435, false, 'Vitaliy', 'Masha', 10];
+//
+// for (let i = 0; i < array.length; i++) {
+//     const arrayElement = array[i];
+//     console.log(arrayElement)
+//
+// }
+
+// // - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+//
+// let array = ['Vasia', 1, 'Petro', 'Bart', true, 435, false, 'Vitaliy', 'Masha', 10];
+//
+// for (let i = 0; i < array.length; i++) {
+//     const arrayElement = array[i];
+//
+//     if (typeof arrayElement === "boolean") {
+//         console.log(arrayElement);
+//     }
+//
+// }
+
+// // - Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки числові елементи
+//
+// let array = ['Vasia', 1, 'Petro', 'Bart', true, 435, false, 'Vitaliy', 'Masha', 10];
+//
+//
+// for (let i = 0; i < array.length; i++) {
+//     const arrayElement = array[i];
+//
+//     if (typeof arrayElement === "number") {
+//         console.log(arrayElement);
+//     }
+//
+// }
+
+// // - Створити масив з 10 елементів числового, стрічкового і булевого типу. За допомогою if та typeof вивести тільки рядкові елементи
+//
+// let array = ['Vasia', 1, 'Petro', 'Bart', true, 435, false, 'Vitaliy', 'Masha', 10];
+//
+// for (let i = 0; i < array.length; i++) {
+//     const arrayElement = array[i];
+//
+//     if (typeof arrayElement === "string") {
+//         console.log(arrayElement);
+//     }
+//
+// }
+
+// // - Створити порожній масив. Наповнити його 10 елементами (різними за типами) через звернення до конкретних індексів. Вивести в консоль всі його елементи в циклі.
+//
+// let array = [];
+//
+// array[0] = 1;
+// array[1] = true;
+// array[2] = 'Vasia';
+// array[3] = 'Katia';
+// array[4] = 4535;
+// array[5] = 67575;
+// array[6] = 'Karina';
+// array[7] = false;
+// array[8] = 489;
+// array[9] = 'Dima';
+// array[10] = 'Masha';
+//
+//
+// for (let i = 0; i < array.length; i++) {
+//     const arrayElement = array[i];
+//     console.log(arrayElement);
+//
+// }
+
+// // Створити цикл for на 10  ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+//
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+//     document.write(`<div>${i}</div>`);
+//
+// }
+
+// // - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+//
+// for (let i = 0; i < 100; i++) {
+//     console.log(i);
+//     document.write(`<div>${i}</div>`);
+//
+// }
+
+// // - Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+//
+// for (let i = 0; i < 100; i+=2) {
+//     document.write(`<div>${i}</div>`);
+//     console.log(i);
+//
+// }
+
+// // - Створити цикл for на 100 ітерацій. Вивести тільки парні кроки. через console.log + document.write
+//
+// for (let i = 1; i < 101; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+//
+// }
+
+// // Створити цикл for на 100 ітерацій. Вивести тільки непарні кроки. через console.log + document.write
+//
+// for (let i = 1; i < 101; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i)
+//     }
+//
+// }
