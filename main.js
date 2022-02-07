17f2985c17c145ce5068715166585b9c1838dea0
nazarko310 <nazar2000pok@gmail.com>
2022-02-07 23:28:25 +0200

    lessons3_add3

--- main.js
+++ main.js
@@ -134,3 +134,97 @@
 // }
 // console.log(arrayNew)
 
+// // - Дано 2 масиви з рівною кількістю об'єктів.
+// // Масиви:
+// // З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
+// //     Записати цей об'єкт в новий масив
+//
+// let usersWithId = [
+//     {id: 1, name: 'vasya', age: 31, status: false },
+//     {id: 2, name: 'petya', age: 30, status: true},
+//     {id: 3, name: 'kolya', age: 29, status: true},
+//     {id: 4, name: 'olya', age: 28, status: false}
+// ];
+//
+// let citiesWithId = [
+//     {user_id: 3, country: 'USA', city: 'Portland'},
+//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//     {user_id: 4, country: 'USA', city: 'Miami'}
+// ];
+//
+// let usersWithCities = [];
+//
+// for (let i = 0; i < usersWithId.length; i++) {
+//     const userWithId = usersWithId[i];
+//     usersWithCities.push(userWithId);
+//     for (let k = 0; k < usersWithCities.length; k++) {
+//         let userWithCity = usersWithCities[k];
+//         for (let j = 0; j < citiesWithId.length; j++) {
+//             let cityWithId = citiesWithId[j];
+//             if (userWithCity.id === cityWithId.user_id) {
+//                 userWithCity.address = cityWithId;
+//
+//             }
+//         }
+//     }
+// }
+//
+// console.log(usersWithCities)
+
+// // - Взяти масив з 10 чисел або створити його. Вивести в консоль тільки ті елементи, значення яких є парними.
+//
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+// for (let i = 0; i < array.length; i++) {
+//     const arrayElement = array[i];
+//     if (arrayElement % 2 === 0) {
+//         console.log(arrayElement);
+//     }
+//
+// }
+
+// // - Взяти масив з 10 чисел або створити його. Створити 2й порожній масив. За допомогою будь-якого циклу скопіювати значення одного масиву в інший.
+//
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+//
+// let arrayNew = [];
+//
+// for (let i = 0; i < array.length; i++) {
+//     let arrayElement = array[i];
+//     arrayNew.push(arrayElement);
+// }
+// console.log(arrayNew);
+
+// // - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for зібрати всі букви в слово.
+//
+// let array = ['a', 'b', 'c'];
+// let str = '';
+// for (let i = 0; i < array.length; i++) {
+//     const arrayElement = array[i];
+//     str += arrayElement;
+// }
+// console.log(str);
+
+// // - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу while зібрати всі букви в слово.
+//
+// let array = ['a', 'b', 'c'];
+//
+// let str = '';
+//
+// let i = 0;
+// while (i < array.length) {
+//     str += array[i];
+//     i++;
+// }
+// console.log(str);
+
+// // - Дано масив: [ 'a', 'b', 'c'] . За допомогою циклу for of зібрати всі букви в слово.
+//
+// let array = ['a', 'b', 'c'];
+// let str = '';
+//
+// for (const arrayElement of array) {
+//     str += arrayElement;
+//
+// }
+// console.log(str);
