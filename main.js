03f004de9f157b802a615269ca6abd8312e9ba2c
nazarko310 <nazar2000pok@gmail.com>
2022-02-10 23:34:41 +0200

    lessons4_cw

--- main.js
+++ main.js
@@ -1,158 +1,153 @@
-// // - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-// let a = +prompt('Введіть ширину');
-// let b = +prompt('Введіть ширину');
+// // створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+// let numberOne = +prompt('Введіть перше число');
+// let numberTwo = +prompt('Введіть друге число');
+// let numberThree = +prompt('Введіть третє число');
 //
-// function square(a, b) {
-//     console.log(a * b);
-//     return a * b;
+// function createMinNumber(numberOne, numberTwo, numberThree) {
+//     if (numberOne < numberTwo && numberOne < numberThree) {
+//         console.log(numberOne);
+//     } else if (numberTwo < numberOne && numberTwo < numberThree) {
+//         console.log(numberTwo);
+//     } else {
+//         console.log(numberThree);
+//     }
+//
 // }
 //
-// square(a, b);
+// createMinNumber(numberOne, numberTwo, numberThree);
 
-// // - створити функцію яка обчислює та повертає площу кола з радіусом r
+// // - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+// let numberOne = +prompt('Введіть перше число');
+// let numberTwo = +prompt('Введіть друге число');
+// let numberThree = +prompt('Введіть третє число');
 //
-// let r = +prompt('Введіть радіус');
+// function createMaxNumber(numberOne, numberTwo, numberThree) {
+//     if (numberOne > numberTwo && numberOne > numberThree) {
+//         console.log(numberOne);
+//     } else if (numberTwo > numberOne && numberTwo > numberThree) {
+//         console.log(numberTwo);
+//     } else {
+//         console.log(numberThree);
+//     }
 //
-// function squareCircle(r) {
-//     console.log(r * 3.14);
-//
-//     return r * 3.14;
 // }
 //
-// squareCircle(r);
+// createMaxNumber(numberOne, numberTwo, numberThree);
 
-// // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
-// let h = +prompt('Введіть висоту');
-// let r = +prompt('Введіть радіус');
+// // - створити функцію яка повертає найбільше число з масиву
 //
-// function squareCylinder(h, r) {
-//     console.log(h * r);
-//     return h * r;
+// let array = [1, 324, 4534, 6757, 453];
+//
+// function arrayCreateMax(array) {
+//     console.log(Math.max.apply(null, array));
 // }
 //
-// squareCylinder(h, r);
+// arrayCreateMax(array);
 
-// // - створити функцію яка приймає масив та виводить кожен його елемент
+// // - створити функцію яка повертає найменьше число з масиву
 //
-// let array = [1, 2, 3, 4, 5, 6];
+// let array = [1, 324, 4534, 6757, 453];
 //
-// function arr(array) {
+// function arrayCreateMin(array) {
+//     console.log(Math.min.apply(null, array));
+// }
+//
+// arrayCreateMin(array);
+
+// // - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+//
+// let array = [1, 324, 4534, 6757, 453];
+// let sum = 0;
+//
+// function arraySum(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//
+//         sum += arr[i];
+//
+//     }
+//     return sum;
+// }
+//
+// console.log(arraySum(array));
+
+// // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+//
+// let array = [1, 324, 4534, 6757, 453];
+// let sum = 0;
+// let average;
+//
+// function createArrayAverage(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         const arrElement = arr[i];
+//         sum += arr[i];
+//         average = sum / arr.length;
+//     }
+//     return average;
+// }
+//
+// console.log(createArrayAverage(array));
+
+// // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+//
+// let array = [489498, 5894, 4849, 4478, 2326, 484, 484, 468];
+//
+// function arrayMaxMin(array) {
+//     let min = array[0];
+//     let max = min;
 //     for (let i = 0; i < array.length; i++) {
-//         const arrayElement = array[i];
-//         console.log(arrayElement);
+//         if (max < array[i]) {
+//             max = array[i];
+//         }
+//         if (min > array[i]) {
+//             min = array[i];
+//         }
+//
+//     }
+//     console.log(max);
+//     return min;
+// }
+//
+// console.log(arrayMaxMin(array));
+
+// // - створити функцію яка заповнює масив рандомними числами
+// let array = [];
+//
+// function createArray(arr) {
+//     for (let i = 0; i < 100; i++) {
+//         arr.push(Math.round(Math.random() * 100))
+//
+//     }
+//     console.log(arr)
+// }
+//
+// createArray(array);
+
+// // - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+//
+// let limit = +prompt('введіть ліміт');
+//
+// let array = [];
+//
+// function createArray(arr) {
+//     for (let i = 0; i < limit; i++) {
+//         arr.push(Math.round(Math.random() * 100))
+//
+//     }
+//     console.log(arr)
+// }
+//
+// createArray(array);
+
+// // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
+//
+// let array = [1, 2, 3];
+//
+// function reverseArray(arr) {
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         const arrElement = arr[i];
+//         console.log(arrElement);
+//
 //     }
 // }
 //
-// arr(array);
-
-// // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
-//
-// let paragraph = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci consequatur ea eius et ex fugiat inventore iste nam, reprehenderit, sequi suscipit, voluptates. Amet aperiam eligendi exercitationem molestias nemo quo reprehenderit.'
-//
-// function createParagraph(p) {
-//     document.write(`<p>${p}</p>`);
-//
-// }
-//
-// createParagraph(paragraph);
-
-// // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
-//
-// let li = 'item';
-//
-// function createItem(li) {
-//     document.write('<ul>');
-//
-//     for (let i = 0; i < 3; i++) {
-//         document.write(`<li>${li}</li>`);
-//
-//     }
-//
-//     document.write('</ul>');
-//
-// }
-//
-// createItem(li);
-
-// // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий. Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
-//
-// let count = +prompt('Введіть к-сть елементів списку');
-//
-// function createItem(text, count) {
-//     document.write('<ul>');
-//
-//     for (let i = 0; i < count; i++) {
-//         document.write(`<li>${text}</li>`);
-//
-//
-//     }
-//
-//     document.write('</ul>')
-// }
-//
-// createItem('item', count);
-
-// // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
-//
-// let array = [1, 'Maks', true, 3, 6, 34243, 'Nazar', false];
-//
-// function createArrayItem(array) {
-//     document.write('<ul>');
-//
-//     for (let i = 0; i < array.length; i++) {
-//         const arrayElement = array[i];
-//         document.write(`<li>${arrayElement}</li>`);
-//
-//     }
-//
-//     document.write('</ul>');
-// }
-//
-// createArrayItem(array);
-
-// // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
-//
-//
-// let users = [
-//     {
-//         id: 1,
-//         name: 'Nazar',
-//         age: 21
-//
-//     },
-//     {
-//         id: 2,
-//         name: 'Maks',
-//         age: 35
-//
-//     },
-//     {
-//         id: 3,
-//         name: 'Dimas',
-//         age: 10
-//
-//     },
-//     {
-//         id: 4,
-//         name: 'Karina',
-//         age: 15
-//
-//     },
-// ]
-//
-//
-// function userCreator(obj) {
-//     document.write(`<div>`);
-//
-//     for (const objElement of obj) {
-//         document.write('<div>');
-//         document.write(`<h1>${objElement.name} №${objElement.id}</h1>`)
-//         document.write(`<p>${objElement.age}</p>`);
-//         document.write('</div>');
-//     }
-//
-//     document.write(`</div>`);
-//
-// }
-//
-// userCreator(users);
+// reverseArray(array);
