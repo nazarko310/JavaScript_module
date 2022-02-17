b23fd9ce85add0a1ac5568239718d26c9c9fd1eb
nazarko310 <nazar2000pok@gmail.com>
2022-02-18 00:55:32 +0200

    lessons5_add

--- main.js
+++ main.js
@@ -1,75 +1,76 @@
-// // - створити функцію, яка якщо приймає один аргумент, просто вивдоить його, якщо два аргументи - складає або конкатенує їх між собою.!!!!!!
+// // - Дано натуральное число n. Выведите все числа от 1 до n.
+// let number = +prompt('Введіть число');
 //
-// function createSentence(stringOne, stringTwo, stringThree) {
-//     for (let i = 0; i < arguments.length; i++) {
-//         if (arguments.length === 1) {
-//             console.log(arguments);
-//         } else {
+// let func = (number) => {
+//     for (let i = 1; i <= number; i++) {
+//         console.log(i);
+//
+//     }
+// }
+//
+// func(number);
+
+// // - Даны два целых числа A и В . Выведите все числа от A до B включительно, в порядке возрастания, если A < B, или в порядке убывания в противном случае.
+//
+//
+// let a = +prompt('A');
+// let b = +prompt('B');
+//
+// let func = (a, b) => {
+//     if (a < b) {
+//         for (let i = a; i <= b; i++) {
+//             console.log(i);
+//         }
+//     } else {
+//         for (let i = a; i>= b; i--) {
+//             console.log(i);
 //
 //         }
 //     }
 // }
 //
-// createSentence('One', 'Two', 'three');
+// func(a, b);
 
-// // створити функцію  яка приймає два масиви та скаладає значення елементів з однаковими індексами  та повертає новий результуючий масив.
+// // - Функція приймає масив та число "i", та міняє місцями об`єкт який знаходиться в індексі "i" на "i+1"
+// let array = [0, 10, 2, 56];
 //
+// function permutationsFunction(array, i) {
+//     let number = 0
+//     let firstNumber = 0;
 //
-// let arrayOne = [1, 2, 3, 4];
-// let arrayTwo = [2, 3, 4, 5];
+//     if (i === (array.length - 1)) {
+//         firstNumber = array[0];
+//         array[0] = array[i];
+//         array[i] = firstNumber;
+//     } else {
+//         number = array[i];
+//         array[i] = array[i + 1];
+//         array[i + 1] = number;
+//     }
+//     console.log(array)
+// }
 //
-// function createArray(arrayOne, arrayTwo) {
+// permutationsFunction(array, 0);
+
+// // - Сворити функцію яка буде переносити елементи з значенням 0 у кінець маисву. Зберігаючи при цьому порядок не нульових значень.
+// let array = [0, 10, 0, 11];
+//
+// function zeroFunction(array) {
+//     let arrayZero = [];
 //     let arrayNew = [];
-//
-//     for (let i = 0; i < arrayOne.length; i++) {
-//         for (let j = 0; j < arrayTwo.length; j++) {
-//             if (i === j) {
-//                 arrayNew.push(arrayOne[i] + arrayTwo[i]);
-//             }
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] === 0) {
+//             arrayZero.push(array[i]);
+//         }
+//         if (array[i] !== 0) {
+//             arrayNew.push(array[i]);
 //         }
 //     }
-//     console.log(arrayNew);
+//     arrayNew = arrayNew.concat(arrayZero);
+//     for (let j = 0; j < arrayNew.length; j++) {
+//         console.log(arrayNew[j]);
+//     }
+//
 // }
 //
-// createArray(arrayOne, arrayTwo);
-
-// // Створити функцію яка приймає масив будь яких объектів, та повертає масив ключів всіх обєктів
-//
-// let data = [{
-//     name: 'Dimas',
-//     age: '13'
-// },
-//     {
-//         model: 'Camry'
-//     }
-// ]
-//
-// function logObject(data) {
-//     for (const element of data) {
-//         console.log(Object.keys(element));
-//     }
-// }
-//
-// logObject(data);
-//
-
-// // Cтворити функцію яка приймає масив будь яких объектів, та повертає масив значень всіх обєктів
-// let data = [
-//     {
-//         name: 'Dimas',
-//         age: '13'
-//     },
-//     {
-//         model: 'Camry'
-//     }
-// ]
-//
-// function logObject(data) {
-//
-//     for (const element of data) {
-//
-//         console.log(Object.values(element));
-//     }
-// }
-//
-// logObject(data);
+// zeroFunction(array);
