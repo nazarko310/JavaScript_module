6eceeb664836f461e0f2e9f7bd36a15859a69b7d
nazarko310 <nazar2000pok@gmail.com>
2022-02-27 18:48:17 +0200

    lessons7_hw

--- main.js
+++ main.js
@@ -1,97 +1,63 @@
-// // - Дано список імен.
-// //     let n1 = 'Harry..Potter'
-// // let n2 = 'Ron---Whisley'
-// // let n3 = 'Hermione__Granger'
-// // Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
-//
-// let n1 = 'Harry..Potter'
-//
-// let funcGenerateName = (n1) => {
-//     let newArray = [];
-//     let arr1 = n1.trim().split('.');
-//     for (let i = 0; i < arr1.length; i++) {
-//         const arr1Element = arr1[i];
-//         if (arr1Element !== "") {
-//             newArray.push(arr1Element);
-//         }
-//     }
-//     return newArray.join(' ')
-//
+// // - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
+// // створити пустий масив, наповнити його 10 об'єктами new User(....)
+// function UserCreator(id, name, surname, email, phone) {
+//     this.id = id;
+//     this.name = name;
+//     this.surname = surname;
+//     this.email = email;
+//     this.phone = phone;
 // }
 //
-// console.log(funcGenerateName(n1));
+// let users = [
+//     new UserCreator(1, 'Vasia', 'Kalinin', 'vasia@gmail.com', '+380688745331'),
+//     new UserCreator(2, 'Masha', 'Petrin', 'vasia@gmail.com', '+380688745331'),
+//     new UserCreator(3, 'Petro', 'Vozniuk', 'vasia@gmail.com', '+380688745331'),
+//     new UserCreator(4, 'Katia', 'Malush', 'vasia@gmail.com', '+380688745331'),
+//     new UserCreator(5, 'Karina', 'Jobden', 'vasia@gmail.com', '+380688745331'),
+//     new UserCreator(6, 'Tom', 'Fredos', 'vasia@gmail.com', '+380688745331'),
+//     new UserCreator(7, 'Dimas', 'Kofgre', 'vasia@gmail.com', '+380688745331'),
+//     new UserCreator(8, 'Vika', 'Dfrefe', 'vasia@gmail.com', '+380688745331'),
+//     new UserCreator(9, 'Nazar', 'Ffref', 'vasia@gmail.com', '+380688745331'),
+//     new UserCreator(10, 'Igor', 'Efrefr', 'vasia@gmail.com', '+380688745331')
+// ]
 
-// // - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
+// // - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
 //
-// let funcSGenerate = () => {
-//     let array = [];
-//     for (let i = 0; i < 10; i++) {
-//         array.push(Math.floor(Math.random() * 100));
-//     }
-//     return array;
-//
-// }
-// const newArray = funcSGenerate();
+// console.log(users.filter(value => value.id % 2 === 0));
 
-// // створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
+// // - Взяти масив з  User[] з попереднього завдання, та відсортувати його по id. по зростанню (sort)
 //
-// newArray.sort((a, b) => a - b)
-// console.log(newArray)
+// console.log(users.sort((a, b) => a.id - b.id));
 
-// // - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter, залишивши тільки парні числа
+//  // - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
+//  // створити пустий масив, наповнити його 10 об'єктами Client
 //
-// console.log(newArray.filter(number => number % 2 === 0));
+//  class Client {
+//      constructor(id, name, surname, email, phone, order) {
+//          this.id = id;
+//          this.name = name;
+//          this.surname = surname;
+//          this.email = email;
+//          this.phone = phone;
+//          this.order = order;
+//      }
+//  }
+//
+//
+//  let clients = [
+//      new Client(1, 'Vasia', 'Kalinin', 'vasia@gmail.com', '+380688745331', ['Nokia', 'Laptop', 'TV']),
+//      new Client(2, 'Vasia', 'Kalinin', 'vasia@gmail.com', '+380688745331', ['Nokia', 'Laptop', 'TV', 'Laptop']),
+//      new Client(3, 'Vasia', 'Kalinin', 'vasia@gmail.com', '+380688745331', ['Nokia', 'Laptop', 'TV', 'Laptop', 'Laptop']),
+//      new Client(4, 'Vasia', 'Kalinin', 'vasia@gmail.com', '+380688745331', ['Nokia', 'Laptop', 'TV', 'Laptop', 'Laptop', 'Laptop']),
+//      new Client(5, 'Vasia', 'Kalinin', 'vasia@gmail.com', '+380688745331', ['Nokia', 'Laptop', 'TV', 'Laptop', 'Laptop', 'Laptop']),
+//      new Client(6, 'Vasia', 'Kalinin', 'vasia@gmail.com', '+380688745331', ['Nokia', 'Laptop', 'TV', 'Laptop', 'Laptop']),
+//      new Client(7, 'Vasia', 'Kalinin', 'vasia@gmail.com', '+380688745331', ['Nokia', 'Laptop', 'TV', 'Laptop', 'Laptop']),
+//      new Client(8, 'Vasia', 'Kalinin', 'vasia@gmail.com', '+380688745331', ['Nokia', 'Laptop', 'TV']),
+//      new Client(9, 'Vasia', 'Kalinin', 'vasia@gmail.com', '+380688745331', ['Nokia', 'Laptop', 'TV']),
+//      new Client(10, 'Vasia', 'Kalinin', 'vasia@gmail.com', '+380688745331', ['Nokia', 'Laptop', 'TV'])
+// ]
 
-// // - створити масив рандомних цілих числових значень (або згенерувати, за допомоги попередньої функції) . за допомоги map та колбеку перетворити всі об'єкти в масиві на стрінгові.
-// newArray.map(number => console.log(number.toString()));
-
-// // - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
-// //     let nums = [11,21,3];
-// // sortNums('ascending') // [3,11,21]
-// // sortNums('descending') // [21,11,3]
+// // - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 //
-// let nums = [11, 21, 3];
 //
-// let direction = prompt('Write direction');
-//
-// let funcSort = (nums, direction) => {
-//     if (direction === 'ascending') {
-//        return  nums.sort((a, b) => a - b);
-//     } else if (direction === 'descending') {
-//        return  nums.sort((a, b) => b - a);
-//
-//     }else {
-//         return 'direction is not used'
-//     }
-// }
-//
-// console.log(funcSort(nums, direction));
-
-// // - є масив
-// // let coursesAndDurationArray = [
-// //     {title: 'JavaScript Complex', monthDuration: 5},
-// //     {title: 'Java Complex', monthDuration: 6},
-// //     {title: 'Python Complex', monthDuration: 6},
-// //     {title: 'QA Complex', monthDuration: 4},
-// //     {title: 'FullStack', monthDuration: 7},
-// //     {title: 'Frontend', monthDuration: 4}
-// // ];
-// // -- відсортувати його за спаданням за monthDuration
-// // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
-//
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
-//
-// let newSortObj = coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration);
-//
-// console.log(newSortObj);
-//
-// let filterNewObj = coursesAndDurationArray.filter(value => value.monthDuration > 5);
-//
-// console.log(filterNewObj);
+// console.log(clients.sort((a, b) => a.order.length - b.order.length))
